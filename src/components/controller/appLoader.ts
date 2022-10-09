60179ea8c3b2c89299a1d5fad455bfc4fe70a69c
Uiguuna <92540313+UiguunaMikhailova@users.noreply.github.com>
2022-10-10 00:57:07 +0900

    Update appLoader.ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'dc1fc624ef0046fc97371261f7d63d38', // получите свой ключ https://newsapi.org/ dc1fc624ef0046fc97371261f7d63d38
         });
     }
